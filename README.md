69441dfb7fd9789599f07ef530b1b91814e833b1
ylee1101 <yongho19911101@hotmail.com>
2018-05-21 08:28:04 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+React native testing app 
+
---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('collision_care_without_login', () => App);
